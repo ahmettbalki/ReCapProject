32cb92e81254b4ef5b5c3642995179f701e75beb
ahmettbalki <ahmettbalki@gmail.com>
2023-04-23 18:05:00 +0300

    AOP used

--- Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
+++ Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -1,13 +1,7 @@
 ﻿using Core.Entities;
 using DataAccess.Abstract;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
-using System.Runtime.ConstrainedExecution;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Core.DataAccess.EntityFramework
 {
